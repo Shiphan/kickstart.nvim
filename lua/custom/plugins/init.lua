20291b6f530e2c2347029d428512b1a68bbcb511
Shiphan <140245703+Shiphan@users.noreply.github.com>
2024-09-10 19:59:01 +0800

    update neotree setting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,30 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    cmd = 'Neotree',
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function()
+            require('neo-tree.command').execute { action = 'close' }
+          end,
+        },
+      },
+    },
+  },
+}
